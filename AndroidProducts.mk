37d5a4272c3a2858ba725bb337615b009a1c819d
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-21 16:55:49 +0600

    @projectblaze

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,15 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The Blaze Project
 # Copyright (C) 2023 Paranoid Android
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mi439.mk
+    $(LOCAL_DIR)/blaze_mi439.mk
+
 
 COMMON_LUNCH_CHOICES := \
-    aosp_mi439-user \
-    aosp_mi439-userdebug \
-    aosp_mi439-eng
+    blaze_mi439-user \
+    blaze_mi439-userdebug \
+    blaze_mi439-eng
